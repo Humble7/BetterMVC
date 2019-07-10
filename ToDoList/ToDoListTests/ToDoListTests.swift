983dc9fe57fb1a81e92c0b6865e352901c74b322
ChenZhen <CZ@123.com>
2019-07-11 07:13:44 +0800

    feat(none): intial a to-do-list project
    The demo is based on MVC.
    It can teach you how to use MVC better.

---
+++ ToDoList/ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,34 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by 陈振 on 2019/7/11.
+//  Copyright © 2019 陈振. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoList
+
+class ToDoListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
